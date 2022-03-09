f918486e11164fa80de6e04df55e829f9d5be240
Tobiwealth <101267703+Tobiwealth@users.noreply.github.com>
2022-03-09 15:51:58 +0100

    adding certain files

---
+++ Script.js
@@ -0,0 +1,20 @@
+// scope
+// root scopr
+var fun = 5;
+
+function funfunction() {
+	var fun = "hellooo";
+	console.log(fun);
+}
+
+function funerfunction() {
+	var fun = "bye";
+	console.log(fun);
+}
+
+function funestfunction() {
+	var fun = "Ahhhhh";
+	console.log(fun);
+}
+
+console.log(fun);
